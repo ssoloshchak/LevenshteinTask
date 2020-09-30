7541983473a367926dadfb4e1d3cb10e6eeb7064
Serhii Soloshchak <ssoloshchak@codifire.com>
2020-09-30 17:33:53 +0300

    Initial.

---
+++ Levenshtein.WebApi/Controllers/LevenshteinDistanceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Levenshtein.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LevenshteinDistanceController : ControllerBase
+    {
+        private readonly ILogger<LevenshteinDistanceController> _logger;
+
+        public LevenshteinDistanceController(ILogger<LevenshteinDistanceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<int>> Get(CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
