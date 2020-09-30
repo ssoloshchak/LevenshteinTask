4c0f585ef1a4b147a46acad55d2ee65823a8955d
Serhii Soloshchak <ssoloshchak@codifire.com>
2020-09-30 18:52:11 +0300

    Logging.

--- Levenshtein.UnitTests/LevenshtainDistanceTest.cs
+++ Levenshtein.UnitTests/LevenshtainDistanceTest.cs
@@ -6,19 +6,21 @@ namespace Levenshtein.UnitTests
 {
     public class LevenshtainDistanceTest
     {
-        private readonly ILevenshteinDistanceCalculation _calculation = new LevenshteinDistanceCalculation();
+        private readonly ILevenshteinDistanceCalculator _calculator = new LevenshteinDistanceCalculator();
 
         [Theory]
-        [InlineData("cat", "flat", 2)]
+        //   [InlineData("cat", "flat", 2)]
+        //[InlineData("flat", "cat", 2)]
+        [InlineData("sik", "sok", 1)]
         //[InlineData("*ike", "trike", 0)]
-        //[InlineData("*ike", "bestbike ", 0)]
+        //[InlineData("*ike", "bbike ", 0)]
         //[InlineData("bike", "*ike", 0)]
         //[InlineData("bike", "*bike", 0)]
         //[InlineData("+ike", "bike", 0)]
         //[InlineData("bike", "+bike", 1)]
         public void DistanceShouldBeEqual(string firstWord, string secondWord, int expectedDistance)
         {
-            int calculatedDistance = _calculation.Calculate(firstWord, secondWord);
+            int calculatedDistance = _calculator.Calculate(firstWord, secondWord);
             Assert.Equal(expectedDistance, calculatedDistance);
         }
     }
--- Levenshtein.WebApi/Infrastructure/LevenshteinDistanceCalculator.cs
+++ Levenshtein.WebApi/Infrastructure/LevenshteinDistanceCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Levenshtein.WebApi.Services;
 
@@ -25,18 +26,49 @@ namespace Levenshtein.WebApi.Infrastructure
             var n = firstWord.Length + 1;
             var m = secondWord.Length + 1;
             var matrixD = new int[n, m];
+            var traceMatrix = new Trace[n, m];
 
             const int deletionCost = 1;
             const int insertionCost = 1;
 
-            for (var i = 0; i < n; i++)
+            matrixD[0, 0] = 0;
+
+            traceMatrix[0, 0] = new Trace
+            {
+                Operation = Operation.None,
+                From = firstWord,
+                To = firstWord,
+                Previous = null,
+                TotalCost = 0,
+                CurrentCost = 0
+            };
+
+            for (var i = 1; i < n; i++)
             {
                 matrixD[i, 0] = i;
+                traceMatrix[i, 0] = new Trace
+                {
+                    TotalCost = i,
+                    CurrentCost = 1,
+                    Operation = Operation.Delete,
+                    Previous = traceMatrix[i - 1, 0],
+                    From = traceMatrix[i - 1, 0].To,
+                    To = traceMatrix[i - 1, 0].To.Remove(0, 1)
+                };
             }
 
-            for (var j = 0; j < m; j++)
+            for (var j = 1; j < m; j++)
             {
                 matrixD[0, j] = j;
+                traceMatrix[0, j] = new Trace
+                {
+                    TotalCost = j,
+                    CurrentCost = 1,
+                    Operation = Operation.Insert,
+                    Previous = traceMatrix[0, j - 1],
+                    From = traceMatrix[0, j - 1].To,
+                    To = traceMatrix[0, j - 1].To.Insert(0, secondWord[j - 1].ToString())
+                };
             }
 
             for (var i = 1; i < n; i++)
@@ -44,18 +76,103 @@ namespace Levenshtein.WebApi.Infrastructure
                 for (var j = 1; j < m; j++)
                 {
                     var substitutionCost = firstWord[i - 1] == secondWord[j - 1] ? 0 : 1;
-                    
-                    matrixD[i, j] = Minimum(matrixD[i - 1, j] + deletionCost,
-                        matrixD[i, j - 1] + insertionCost,
-                        matrixD[i - 1, j - 1] + substitutionCost);
+
+                    //solving the elementary task: by deciding which way to take
+                    var deletionTotalCost = matrixD[i - 1, j] + deletionCost;
+                    var insertionTotalCost = matrixD[i, j - 1] + insertionCost;
+                    var substitutionTotalCost = matrixD[i - 1, j - 1] + substitutionCost;
+
+                    if (deletionTotalCost <= insertionTotalCost && deletionTotalCost <= substitutionTotalCost)
+                    {
+                        matrixD[i, j] = deletionTotalCost;
+                        var previous = traceMatrix[i - 1, j];
+                        traceMatrix[i, j] = new Trace
+                        {
+                            CurrentCost = deletionCost,
+                            TotalCost = deletionTotalCost,
+                            Previous = previous,
+                            Operation = Operation.Delete,
+                            From = previous.To,
+                            To = previous.To.Remove(j, 1)
+                        };
+                    }
+                    else if (insertionTotalCost <= deletionTotalCost && insertionTotalCost <= substitutionTotalCost)
+                    {
+                        matrixD[i, j] = insertionTotalCost;
+                        var previous = traceMatrix[i, j - 1];
+                        traceMatrix[i, j] = new Trace
+                        {
+                            CurrentCost = insertionCost,
+                            TotalCost = insertionTotalCost,
+                            Previous = previous,
+                            Operation = Operation.Insert,
+                            From = previous.To,
+                            To = previous.To.Insert(i, secondWord[j - 1].ToString())
+                        };
+                    }
+                    else
+                    {
+                        matrixD[i, j] = substitutionTotalCost;
+                        var previous = traceMatrix[i - 1, j - 1];
+                        traceMatrix[i, j] = new Trace
+                        {
+                            CurrentCost = substitutionCost,
+                            TotalCost = substitutionTotalCost,
+                            Previous = previous,
+                            Operation = Operation.Update,
+                            From = previous.To,
+                            To = substitutionCost == 0 ? previous.To 
+                                : previous.To.Insert(j - 1, secondWord[j - 1].ToString()).Remove(j, 1)
+
+                        };
+                    }
                 }
             }
+
+            var lastTrace = traceMatrix[n - 1, m - 1];
+            var changeLog = GetChangeLog(lastTrace);
 
             return matrixD[n - 1, m - 1];
         }
 
+        private List<string> GetChangeLog(Trace trace)
+        {
+            var changeLog = new Stack<string>();
+
+            while (trace.Previous != null)
+            {
+                if (trace.CurrentCost > 0 && trace.Operation != Operation.None)
+                    changeLog.Push(trace.ToString());
+
+                trace = trace.Previous;
+            }
+
+            return changeLog.ToList();
+        }
+        public class Trace
+        {
+            public int CurrentCost { get; set; }
+            public int TotalCost { get; set; }
+            public Trace Previous { get; set; }
+            public Operation Operation { get; set; }
+            public string From { get; set; }
+            public string To { get; set; }
+
+            public override string ToString()
+            {
+                return $"{From} => {To} | {Enum.GetName(typeof(Operation), Operation)} | Cost: {CurrentCost} | Total cost: {TotalCost}";
+            }
+        }
+
+        public enum Operation
+        {
+            None = 0,
+            Insert = 1,
+            Update = 2,
+            Delete = 3
+        }
+
         private bool ContainsWildCard(string value) => value.Any(t => t == '*' || t == '+');
-        private int Minimum(int a, int b, int c) => (a = a < b ? a : b) < c ? a : c;
     }
 
 }
